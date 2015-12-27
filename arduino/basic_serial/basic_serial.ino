73c6e708b58c51f444ac90cfec95cded71cb0c6e
Alex Graff <agraff@users.noreply.github.com>
2015-12-27 18:18:22 +0000

    Use internal pull-up resistors for digital input.

--- arduino/basic_serial/basic_serial.ino
+++ arduino/basic_serial/basic_serial.ino
@@ -1,21 +1,32 @@
 int switchPin = 10;
 int ledPin = 13;
+int PUSHED = LOW;
 
 void setup() {
-  pinMode(switchPin, INPUT);
+  pinMode(switchPin, INPUT_PULLUP);
   pinMode(ledPin, OUTPUT);
   
   Serial.begin(9600);
 }
 
 void loop() {
-  if (digitalRead(switchPin) == HIGH) {
+  if (digitalRead(switchPin) == PUSHED) {
     digitalWrite(ledPin, HIGH);
-    Serial.write(1);
+    serialWrite(1);
   } else {
     digitalWrite(ledPin, LOW);
-    Serial.write(0);
+    serialWrite(0);
   }
   delay(100);
+}
+
+void serialWrite(int value) {
+  bool useBinary = true;
+  
+  if (useBinary) {
+    Serial.write(value);
+  } else {
+    Serial.print(value);
+  }
 }
 
